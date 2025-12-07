c0fc95fb17f9025b5b00a68548f3a2a7d967b4fe
Atul Kamble <atul_kamble@hotmail.com>
2025-12-07 11:13:31 +0530

    uodate

--- send_message.py
+++ send_message.py
@@ -9,7 +9,7 @@ queue_url = sqs.get_queue_url(QueueName=queue_name)['QueueUrl']
 # Send message
 response = sqs.send_message(
     QueueUrl=queue_url,
-    MessageBody="Hello from Cloudnautic SQS Project!"
+    MessageBody="Hello from SQS Project!"
 )
 
 print("Message sent! Message ID:", response['MessageId'])
